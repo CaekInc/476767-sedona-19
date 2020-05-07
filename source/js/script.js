0a7b1956948804a84dea6cc6f51412ae5909314f
Caek <caekinc@gmail.com>
2020-05-07 13:51:46 +0300

    add script

--- source/js/script.js
+++ source/js/script.js
@@ -1,2 +1,23 @@
 let buttonTogle = document.querySelector('.page-header__toggle');
-console.log(buttonTogle);
+let navHidden = document.querySelector('.header-nav_hidden');
+let navToggle = document.querySelector('.header-nav__toggle');
+
+
+navHidden.classList.remove('header-nav_nojs');
+
+buttonTogle.addEventListener('click', function() {
+  if (navHidden.classList.contains('header-nav_hidden')) {
+    navHidden.classList.remove('header-nav_hidden');
+    navHidden.classList.add('header-nav');
+  } else {
+    navHidden.classList.remove('header-nav');
+    navHidden.classList.add('header-nav_hidden');
+  }
+});
+
+navToggle.addEventListener('click', function () {
+  if (navHidden.classList.contains('header-nav')) {
+    navHidden.classList.remove('header-nav');
+    navHidden.classList.add('header-nav_hidden');
+  }
+});
