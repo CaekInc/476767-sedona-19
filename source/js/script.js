0b346eca79bc346e0f9ffc3a7de80350b784da89
Caek <caekinc@gmail.com>
2020-05-11 11:10:46 +0300

    change name

--- source/js/script.js
+++ source/js/script.js
@@ -1,23 +1,23 @@
 let buttonTogle = document.querySelector('.page-header__toggle');
-let navHidden = document.querySelector('.header-nav_hidden');
+let navHidden = document.getElementById('navigation');
 let navToggle = document.querySelector('.header-nav__toggle');
 
 
 navHidden.classList.remove('header-nav_nojs');
 
 buttonTogle.addEventListener('click', function() {
-  if (navHidden.classList.contains('header-nav_hidden')) {
-    navHidden.classList.remove('header-nav_hidden');
-    navHidden.classList.add('header-nav');
-  } else {
+  if (navHidden.classList.contains('header-nav')) {
     navHidden.classList.remove('header-nav');
     navHidden.classList.add('header-nav_hidden');
+  } else {
+    navHidden.classList.remove('header-nav_hidden');
+    navHidden.classList.add('header-nav');
   }
 });
 
 navToggle.addEventListener('click', function () {
-  if (navHidden.classList.contains('header-nav')) {
-    navHidden.classList.remove('header-nav');
-    navHidden.classList.add('header-nav_hidden');
+  if (navHidden.classList.contains('header-nav_hidden')) {
+    navHidden.classList.remove('header-nav_hidden');
+    navHidden.classList.add('header-nav');
   }
 });
