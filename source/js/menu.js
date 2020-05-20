95a2baa22ccc52df9a565204f3372798c8817501
Caek <caekinc@gmail.com>
2020-05-20 20:23:56 +0300

    add some script

--- source/js/menu.js
+++ source/js/menu.js
@@ -1,10 +1,13 @@
 var buttonTogle = document.getElementById('buttonShow');
 var navHidden = document.getElementById('navigation');
 var navToggle = document.getElementById('buttonHide');
-
+//  popup js
 var popupSucces = document.getElementById('popupSucces');
 var popupFail = document.getElementById('popupFail');
-
+var formVisitors = document.getElementById('form-visitors');
+var formButton = document.getElementById('visitors-button');
+var closeSucces = document.getElementById('succes-close');
+var closeFail = document.getElementById('fail-close');
 
 navHidden.classList.remove('header-nav_nojs');
 
@@ -22,5 +25,27 @@ navToggle.addEventListener('click', function () {
   if (navHidden.classList.contains('header-nav_hidden')) {
     navHidden.classList.remove('header-nav_hidden');
     navHidden.classList.add('header-nav_visible');
+  }
+});
+
+
+//  popup js
+
+
+formVisitors.addEventListener('submit', function (evt) {
+  evt.preventDefault();
+  popupSucces.classList.add('popup_show-succes');
+  closeSucces.focus();
+});
+
+closeSucces.addEventListener('click', function () {
+  if (popupSucces.classList.contains('popup_show-succes')) {
+    popupSucces.classList.remove('popup_show-succes');
+  }
+});
+
+closeFail.addEventListener('click', function () {
+  if (popupFail.classList.contains('popup_show-fail')) {
+    popupFail.classList.remove('popup_show-fail');
   }
 });
